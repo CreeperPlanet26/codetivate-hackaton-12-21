fe1ce7240b54e764a23cb2e6ea592ba944b2a21b
Srikark-17 <srikar.sgk@gmail.com>
2021-12-19 15:20:26 -0600

    Added Transparent Logo

--- src/modules/home/HomePage.tsx
+++ src/modules/home/HomePage.tsx
@@ -6,50 +6,50 @@ import { NavBar } from "../../ui/NavBar";
 import { LoginButton } from "../../ui/LoginButton";
 import { HelpPortalButton } from "../../ui/HelpPortalButton";
 
-
 export const HomePage = () => {
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    return (
-        <div className="home-page">
-            <NavBar isOpen={isOpen} setIsOpen={setIsOpen} />
-            <nav>
-                <Link to="/">
-                    <Button className="tts-button">Assist</Button>
-                </Link>
+  return (
+    <div className="home-page">
+      <NavBar isOpen={isOpen} setIsOpen={setIsOpen} />
+      <nav>
+        <Link to="/">
+          <Button className="tts-button">Assist</Button>
+        </Link>
 
-                <div className="links">
-                    {navLinks.map((link) => (
-                        <Link key={link.path} to={link.path}>
-                            <Button className="a">{link.name}</Button>
-                        </Link>
-                    ))}
-                </div>
-
-                <Button onClick={() => setIsOpen(true)} className="hamburger">
-                    <img src="/assets/hamburger.svg" />
-                </Button>
-            </nav>
-
-
-            <div className="grid">
-                <div className="text">
-                    <h1>Assist</h1>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam quae velit provident aperiam itaque accusantium eveniet sunt labore quod at.
-                    </p>
-                    <section>
-                        <div className="login-button">
-                            <LoginButton />
-                        </div>
-
-                        <div className="join-button">
-                            <HelpPortalButton />
-                        </div>
-                    </section>
-                </div>
-                <img className="art" src="/assets/art.svg" />
-            </div>
+        <div className="links">
+          {navLinks.map((link) => (
+            <Link key={link.path} to={link.path}>
+              <Button className="a">{link.name}</Button>
+            </Link>
+          ))}
         </div>
-    );
+
+        <Button onClick={() => setIsOpen(true)} className="hamburger">
+          <img src="/assets/hamburger.svg" />
+        </Button>
+      </nav>
+
+      <div className="grid">
+        <div className="text">
+          <h1>Assist</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam quae
+            velit provident aperiam itaque accusantium eveniet sunt labore quod
+            at.
+          </p>
+          <section>
+            <div className="login-button">
+              <LoginButton />
+            </div>
+
+            <div className="join-button">
+              <HelpPortalButton />
+            </div>
+          </section>
+        </div>
+        <img className="art" src="/assets/art.svg" />
+      </div>
+    </div>
+  );
 };
