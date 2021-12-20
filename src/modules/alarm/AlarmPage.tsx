3f22135ce52a94c3007ea2f0faa8b41bb59fbcf3
backpack455 <39465356+backpack455@users.noreply.github.com>
2021-12-20 18:55:32 -0500

    Almost done, pending is integration with table

--- src/modules/alarm/AlarmPage.tsx
+++ src/modules/alarm/AlarmPage.tsx
@@ -1,53 +1,227 @@
-// export const AlarmPage = () => {
-//     return (
-//         <div className="alarm-page">
-//             <h2>alarm page</h2>
-//         </div>
-//     )
-// }
+// @ts-nocheck
 import React from "react";
-// import axios from "axios";
-//import { upload } from "@testing-library/user-event/dist/upload";
+import ReactDOM from 'react-dom'
+import Button from '@mui/material/Button'
+import { styled } from '@mui/material/styles';
+import IconButton from '@mui/material/IconButton';
+import PhotoCamera from '@mui/icons-material/PhotoCamera';
+import Stack from '@mui/material/Stack';
+import { createTheme } from '@mui/material/styles';
+import PropTypes from 'prop-types';
+import Box from '@mui/material/Box';
+import Collapse from '@mui/material/Collapse';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+
 
 export default function AlarmPage() {
   const [uploadFile, setUploadFile] = React.useState();
-  const [superHero, setSuperHero] = React.useState();
-  
+  const [predictions, setPredictions] =  React.useState();
+
+  const theme = createTheme({
+    palette: {
+      primary: {
+        light: '#757ce8',
+        main: '#3f50b5',
+        dark: '#002884',
+        contrastText: '#fff',
+      },
+      secondary: {
+        light: '#ff7961',
+        main: '#f44336',
+        dark: '#ba000d',
+        contrastText: '#000',
+      },
+    },
+  });
   const submitForm = (event) => {
     event.preventDefault();
 
     const dataArray = new FormData();
-    //dataArray.append("superHeroName", superHero);
-    // dataArray.append("recording", uploadFile[0]);
-	// console.log(uploadFile[0])
-	// var formdata = new FormData();
-	// formdata.append("recording", uploadFile[0], "gunfire.wav");
+    // dataArray.append("superHeroName", superHero);
+    dataArray.append("recording", uploadFile[0]);
+	console.log(uploadFile[0])
+	var formdata = new FormData();
+	formdata.append("recording", uploadFile[0], "gunfire.wav");
 
-	// var requestOptions = {
-	// method: 'POST',
-	// body: formdata,
-	// redirect: 'follow'
-	// };
+	var requestOptions = {
+	method: 'POST',
+	body: formdata,
+	redirect: 'follow'
+	};
 
-	// fetch("http://127.0.0.1:5000/upload", requestOptions)
-	// .then(response => response.text())
-	// .then(result => console.log(result))
-	// .catch(error => console.log('error', error));
+	fetch("http://127.0.0.1:5000/upload", requestOptions)
+	.then(response => response.text())
+	.then(result => console.log(result))
+	.catch(error => console.log('error', error));
   };
+  const Input = styled('input')({
+    display: 'none',
+  });
 
+  function createData(name, calories, fat, carbs, protein, price) {
+    return {
+      name,
+      calories,
+      fat,
+      carbs,
+      protein,
+      price,
+      history: [
+        {
+          date: '2020-01-05',
+          customerId: '11091700',
+          amount: 3,
+        },
+        {
+          date: '2020-01-02',
+          customerId: 'Anonymous',
+          amount: 1,
+        },
+      ],
+    };
+  }
+  
+  function Row(props) {
+    const { row } = props;
+    const [open, setOpen] = React.useState(false);
+  
+    return (
+      <React.Fragment>
+        <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
+          <TableCell>
+            <IconButton
+              aria-label="expand row"
+              size="small"
+              onClick={() => setOpen(!open)}
+            >
+              {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+            </IconButton>
+          </TableCell>
+          <TableCell component="th" scope="row">
+            {row.name}
+          </TableCell>
+          <TableCell align="right">{row.calories}</TableCell>
+          <TableCell align="right">{row.fat}</TableCell>
+          <TableCell align="right">{row.carbs}</TableCell>
+          <TableCell align="right">{row.protein}</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+            <Collapse in={open} timeout="auto" unmountOnExit>
+              <Box sx={{ margin: 1 }}>
+                <Typography variant="h6" gutterBottom component="div">
+                  History
+                </Typography>
+                <Table size="small" aria-label="purchases">
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>Date</TableCell>
+                      <TableCell>Customer</TableCell>
+                      <TableCell align="right">Amount</TableCell>
+                      <TableCell align="right">Total price ($)</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {row.history.map((historyRow) => (
+                      <TableRow key={historyRow.date}>
+                        <TableCell component="th" scope="row">
+                          {historyRow.date}
+                        </TableCell>
+                        <TableCell>{historyRow.customerId}</TableCell>
+                        <TableCell align="right">{historyRow.amount}</TableCell>
+                        <TableCell align="right">
+                          {Math.round(historyRow.amount * row.price * 100) / 100}
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </Box>
+            </Collapse>
+          </TableCell>
+        </TableRow>
+      </React.Fragment>
+    );
+  }
+  
+  Row.propTypes = {
+    row: PropTypes.shape({
+      calories: PropTypes.number.isRequired,
+      carbs: PropTypes.number.isRequired,
+      fat: PropTypes.number.isRequired,
+      history: PropTypes.arrayOf(
+        PropTypes.shape({
+          amount: PropTypes.number.isRequired,
+          customerId: PropTypes.string.isRequired,
+          date: PropTypes.string.isRequired,
+        }),
+      ).isRequired,
+      name: PropTypes.string.isRequired,
+      price: PropTypes.number.isRequired,
+      protein: PropTypes.number.isRequired,
+    }).isRequired,
+  };
+  
+  const rows = [
+    createData('Frozen yoghurt', 159, 6.0, 24, 4.0, 3.99),
+    createData('Ice cream sandwich', 237, 9.0, 37, 4.3, 4.99),
+    createData('Eclair', 262, 16.0, 24, 6.0, 3.79),
+    createData('Cupcake', 305, 3.7, 67, 4.3, 2.5),
+    createData('Gingerbread', 356, 16.0, 49, 3.9, 1.5),
+  ];
   return (
     <div>
-      <form onSubmit={submitForm}>
-        {/* <input
-          type="text"
-          onChange={(e) => setSuperHero(e.target.value)}
-          placeholder={"Superhero Name"}
-        /> */}
+    {/* <form onSubmit={submitForm}> */}
         <br />
         {/* <input type="file" onChange={(e) => setUploadFile(e.target.files)} /> */}
+        <label htmlFor="contained-button-file">
+        <Input accept="wav/*" id="contained-button-file" multiple type="file" onChange={(e) => setUploadFile(e.target.files)}  />
+        <Button color="primary" variant="contained" component="span">
+            Upload
+        </Button>
+        </label>
+        <label htmlFor="icon-button-file">
+        <br/>
+        {/* <Input accept="image/*" id="icon-button-file" type="file" />
+        <IconButton color="primary" aria-label="upload picture" component="span">
+            <PhotoCamera />
+        </IconButton> */}
+        </label>
         <br />
-        <input type="submit" />
-      </form>
+        <Button variant="outlined"onClick={submitForm}>
+        Click me
+        </Button>
+        {/* <input type="submit" /> */}
+      {/* </form> */}
+      <TableContainer component={Paper}>
+      <Table aria-label="collapsible table">
+        <TableHead>
+          <TableRow>
+            <TableCell />
+            <TableCell>Dessert (100g serving)</TableCell>
+            <TableCell align="right">Calories</TableCell>
+            <TableCell align="right">Fat&nbsp;(g)</TableCell>
+            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <Row key={row.name} row={row} />
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+
     </div>
   );
 }
