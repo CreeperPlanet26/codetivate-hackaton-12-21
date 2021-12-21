ec2c5dd494c9cee90d1b503671b7957769cef560
CreeperPlanet26 <fortniteplanet26@gmail.com>
2021-12-21 10:47:28 -0500

    make get data automatic

--- src/PoliceDashboard/PoliceDashboard.tsx
+++ src/PoliceDashboard/PoliceDashboard.tsx
@@ -5,10 +5,6 @@ import { useHistory } from "react-router-dom";
 const PoliceDashboard = () => {
   const history = useHistory();
 
-  // Look at my onClick Function
-  // The reason it wasn't working before is because the doc "dfsa" doesn't have any info inside of it, only a subcollection.
-  // That's why I had to go into the specific subcollection and get the info
-
   return (
     <div className="police-dashboard-component">
       <Map />
@@ -20,23 +16,6 @@ const PoliceDashboard = () => {
         }
       >
         SignOut
-      </button>
-      <button
-        onClick={() =>
-          db
-            .collection("schools")
-            .doc("dsfa")
-            .collection("evacuation_update")
-            .get()
-            .then((querySnapshot) => {
-              querySnapshot.forEach((doc) => {
-                // doc.data() is never undefined for query doc snapshots
-                console.log(doc.id, " => ", doc.data());
-              });
-            })
-        }
-      >
-        Get info
       </button>
     </div>
   );
